6c6d0b6a3e93a432eed602b6092b7e645d6c8b33
Kep <KepCreations@gmail.com>
2022-01-18 22:30:42 -0500

    Create getCurrentUser and UserContext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 import JoblyApi from './api/api';
 import './App.css';
 import Navbar from './routes/Navbar';
 import Routes from './routes/Routes';
+import UserContext from './auth/UserContext';
 
-function App() {
+const App = () => {
 	const [ token, setToken ] = useState(null);
+	const [ currentUser, setCurrentUser ] = useState(null);
 
-	console.log(token);
+	useEffect(
+		function loadUserData() {
+			const getCurrentUser = async () => {
+				if (token) {
+					try {
+						let { username } = jwt.decode(token);
+						JoblyApi.token = token;
+						let currentUser = await JoblyApi.getCurrentUser(username);
+						setCurrentUser(currentUser);
+					} catch (err) {
+						console.error('Error loading user data', err);
+						setCurrentUser(null);
+					}
+				}
+			};
+			getCurrentUser();
+		},
+		[ token ]
+	);
 
 	const logout = () => {
 		setToken(null);
+		setCurrentUser(null);
 	};
 
 	const login = async (loginData) => {
@@ -40,12 +61,14 @@ function App() {
 
 	return (
 		<BrowserRouter>
-			<div className="App">
-				<Navbar logout={logout} />
-				<Routes signup={signup} login={login} />
-			</div>
+			<UserContext.Provider value={{ currentUser, setCurrentUser }}>
+				<div className="App">
+					<Navbar logout={logout} />
+					<Routes signup={signup} login={login} />
+				</div>
+			</UserContext.Provider>
 		</BrowserRouter>
 	);
-}
+};
 
 export default App;
