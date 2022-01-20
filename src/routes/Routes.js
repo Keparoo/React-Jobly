afa614ca22e76b27b524cfd413722311064c6c33
Kep <KepCreations@gmail.com>
2022-01-20 15:56:16 -0500

    Cleanup routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,38 +1,60 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Homepage from '../homepage/Homepage';
+import SignupForm from '../auth/SignupForm';
+import LoginForm from '../auth/LoginForm';
 import CompanyList from '../companies/CompanyList';
 import CompanyDetails from '../companies/CompanyDetails';
 import JobList from '../jobs/JobList';
-import SignupForm from '../auth/SignupForm';
-import LoginForm from '../auth/LoginForm';
 import Profile from '../profiles/ProfileForm';
-import Homepage from '../homepage/Homepage';
 import LoggedInRoute from './LoggedInRoute';
 
+/* Routes for site
+
+  Routes wrapped in LoggedInRoute require user to be logged in else
+  redirect to login page
+
+  Non-existant routes route to homepage
+
+*/
+
 const Routes = ({ login, signup }) => {
+	console.debug(
+		'Routes',
+		`login=${typeof login}`,
+		`register=${typeof register}`
+	);
+
 	return (
 		<Switch>
-			<LoggedInRoute exact path="/companies">
-				<CompanyList />
-			</LoggedInRoute>
-			<LoggedInRoute exact path="/companies/:handle">
-				<CompanyDetails />
-			</LoggedInRoute>
-			<LoggedInRoute exact path="/jobs">
-				<JobList />
-			</LoggedInRoute>
-			<Route exact path="/signup">
-				<SignupForm signup={signup} />
-			</Route>
-			<Route exact path="/login">
-				<LoginForm login={login} />
-			</Route>
-			<LoggedInRoute exact path="/profile">
-				<Profile />
-			</LoggedInRoute>
 			<Route exact path="/">
 				<Homepage />
 			</Route>
+
+			<Route exact path="/signup">
+				<SignupForm signup={signup} />
+			</Route>
+
+			<Route exact path="/login">
+				<LoginForm login={login} />
+			</Route>
+
+			<LoggedInRoute exact path="/companies">
+				<CompanyList />
+			</LoggedInRoute>
+
+			<LoggedInRoute exact path="/companies/:handle">
+				<CompanyDetails />
+			</LoggedInRoute>
+
+			<LoggedInRoute exact path="/jobs">
+				<JobList />
+			</LoggedInRoute>
+
+			<LoggedInRoute exact path="/profile">
+				<Profile />
+			</LoggedInRoute>
 
 			<Redirect to="/" />
 		</Switch>
